8770d30a28a31c8b59ec3ca817a7aa963819f1f0
Josh Darling <Joshua.a.darling@gmail.com>
2022-10-27 13:39:12 -0600

    part 2 complete starting testing and bonus points

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,17 @@
-function addCommas() {}
+function addCommas(num) {
+    const numToArr = new Array(...num.toString())
+    const idx = numToArr.length - 1
+    for(let i = numToArr.length; i > 0; i -= 3) {
+        numToArr.splice(i, 0, ',')
+       
+    }
+
+   
+   numToArr.pop()
+   console.log(numToArr.join(''))
+
+}
+
+addCommas(1000000)
 
 module.exports = addCommas;
